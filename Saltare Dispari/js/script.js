4c845f866742bfe95b33a5e1a5554383a11d96e9
raghveerdeol <deolraghveer5@gmail.com>
2024-05-13 22:17:31 +0200

    aggiunti tutti i link alle paggine index html e stile nel css

--- Saltare Dispari/js/script.js
+++ Saltare Dispari/js/script.js
@@ -3,6 +3,7 @@
 
 // array vuoto
 const x = [];
+let mostra = document.querySelector("main > div");
 // input number for i < 6
 for (let i = 0; i < 6; i++) {
     const number = Number.parseInt(prompt("inserire un numero"));
@@ -10,8 +11,10 @@ for (let i = 0; i < 6; i++) {
     if (number % 2 == 0) {
         // inserire nel array
         x.push(number);
+        mostra.innerHTML = x;
         console.log(x);
     } else{
+        mostra.innerHTML = x;
         console.log(x);
     }
     
