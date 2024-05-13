2d8dd7f000666359fc167cce8e1473bd2b528763
raghveerdeol <deolraghveer5@gmail.com>
2024-05-13 19:18:09 +0200

    aggiunto reset e decorata la pagina

--- Email/js/script.js
+++ Email/js/script.js
@@ -7,12 +7,12 @@ let userEmail = document.getElementById("email");
 const submit = document.getElementById("submit");
 const emailUtenti = ['ale@mail.com', 'davide@mail.com', 'jo@mail.com', 'zoro@mail.com'];
 let accesso = document.getElementById("accesso");
-
+let reset = document.getElementById("reset");
 
 
 submit.addEventListener('click', function() {
     console.log(userEmail.value);
-    
+
     if (emailUtenti.includes(userEmail.value) ) {
     accesso.innerHTML = "Può accedere";
     console.log('può accedere');
@@ -22,3 +22,8 @@ submit.addEventListener('click', function() {
 }
 })
 console.log(emailUtenti);
+
+reset.addEventListener('click', function () {
+    userEmail.value = "";
+    accesso.innerHTML = "";
+})
