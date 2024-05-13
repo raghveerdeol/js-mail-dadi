53c09af25bad4665bd57d0e1c7f85195a5b1eca1
raghveerdeol <deolraghveer5@gmail.com>
2024-05-13 18:57:18 +0200

    aggiunto eventListener

--- Email/js/script.js
+++ Email/js/script.js
@@ -2,17 +2,23 @@
 input email utente.
 lista array con email di chi puo accedere.
 se email presente nel array stampare valido se no stampare non valido. */
+
 let userEmail = document.getElementById("email");
-let accesso = document.getElementById("accesso");
-userEmail = prompt("inserire l\'email");
+const submit = document.getElementById("submit");
 const emailUtenti = ['ale@mail.com', 'davide@mail.com', 'jo@mail.com', 'zoro@mail.com'];
+let accesso = document.getElementById("accesso");
 
-console.log(emailUtenti);
 
-if (emailUtenti.includes(userEmail) ) {
+
+submit.addEventListener('click', function() {
+    console.log(userEmail.value);
+    
+    if (emailUtenti.includes(userEmail.value) ) {
     accesso.innerHTML = "Può accedere";
     console.log('può accedere');
 } else {
     accesso.innerHTML = "Non può accedere";
     console.log('non può accedere');
 }
+})
+console.log(emailUtenti);
