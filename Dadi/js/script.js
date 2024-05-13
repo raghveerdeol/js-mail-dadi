efcc0e2814703699c09ba99bf894fe7bbbb15157
raghveerdeol <deolraghveer5@gmail.com>
2024-05-13 16:04:44 +0200

    aggiunto if per stampare risultati partita

--- Dadi/js/script.js
+++ Dadi/js/script.js
@@ -3,7 +3,14 @@
 // gnerare 2 numeri random da 1 a 6,
 const userNumber = Math.round(Math.random() * 5 + 1);
 const computerNumber = Math.round(Math.random() * 5 + 1);
-// il numero più grande vince
-
-
 console.log(userNumber, computerNumber)
+// il numero più grande vince
+if (userNumber === computerNumber) {
+    console.log('Partita in stallo');
+} else if (userNumber > computerNumber) {
+    console.log('Il giocatore ha vinto');
+} else {
+    console.log('IL computer ha vinto');
+}
+
+
