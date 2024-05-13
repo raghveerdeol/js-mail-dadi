a8a237e4ec7a3b1b5568d4f65338ee4c99b1dd34
raghveerdeol <deolraghveer5@gmail.com>
2024-05-13 18:29:34 +0200

    aggiunto array email e emailUtenti.includes(usermail)

--- Email/js/script.js
+++ Email/js/script.js
@@ -2,13 +2,17 @@
 input email utente.
 lista array con email di chi puo accedere.
 se email presente nel array stampare valido se no stampare non valido. */
-
-const userEmail = prompt("inserire l\'email");
+let userEmail = document.getElementById("email");
+let accesso = document.getElementById("accesso");
+userEmail = prompt("inserire l\'email");
 const emailUtenti = ['ale@mail.com', 'davide@mail.com', 'jo@mail.com', 'zoro@mail.com'];
+
 console.log(emailUtenti);
 
 if (emailUtenti.includes(userEmail) ) {
+    accesso.innerHTML = "Può accedere";
     console.log('può accedere');
 } else {
+    accesso.innerHTML = "Non può accedere";
     console.log('non può accedere');
 }
