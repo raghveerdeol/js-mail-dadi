4639ab675bd0eff6fd3145b438749580a15282a5
raghveerdeol <deolraghveer5@gmail.com>
2024-05-13 16:59:01 +0200

    aggiunta la partita al dom

--- Dadi/js/script.js
+++ Dadi/js/script.js
@@ -5,12 +5,21 @@ const userNumber = Math.round(Math.random() * 5 + 1);
 const computerNumber = Math.round(Math.random() * 5 + 1);
 console.log(userNumber, computerNumber)
 // il numero più grande vince
+const userElement = document.getElementById("usernumber");
+const computerElement = document.getElementById("computernumber");
+const risultato = document.getElementById("risultato")
+
 if (userNumber === computerNumber) {
+    risultato.innerHTML = "Partita in stallo";
     console.log('Partita in stallo');
+    
 } else if (userNumber > computerNumber) {
+    risultato.innerHTML = "Il giocatore ha vinto"
     console.log('Il giocatore ha vinto');
 } else {
+    risultato.innerHTML = "Il computer ha vinto"
     console.log('IL computer ha vinto');
 }
 
-
+userElement.innerHTML = userNumber;
+computerElement.innerHTML = computerNumber;
