84a4f4bd73f53a55a54388733f7af629cf68c922
raghveerdeol <deolraghveer5@gmail.com>
2024-05-13 15:58:39 +0200

    added 2 random number to 2 const

---
+++ Dadi/js/script.js
@@ -0,0 +1,9 @@
+
+
+// gnerare 2 numeri random da 1 a 6,
+const userNumber = Math.round(Math.random() * 5 + 1);
+const computerNumber = Math.round(Math.random() * 5 + 1);
+// il numero più grande vince
+
+
+console.log(userNumber, computerNumber)
